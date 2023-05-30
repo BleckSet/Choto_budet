95cff6394382be0d3a28709554a4b43578030477
Dunik <bleckset@bk.ru>
2023-05-30 17:35:34 +0300

    fix

--- js/main.js
+++ js/main.js
@@ -23,19 +23,61 @@ function getRandomOffset() {
 }
 
 
+2
+
+  
+$('.product_slider').slick({
+  slidesToShow: 2,
+  centerMode: true,
+  centerPadding: '20px',
+  slidesToScroll: 1,
+  dots: false,
+  arrow:true,
+  infinite: true,
+  variableWidth:true,
+   responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        centerMode: false,
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        variableWidth: false
+      }
+    }
+    
+  ],
+  prevArrow: $('.custom_btn_slider_left'),
+  nextArrow: $('.custom_btn_slider_right')
+  
+});
+
+$('.recomen_slider').slick({
+  slidesToShow: 5.5,
+  slidesToScroll: 1,
+  dots: false,
+  arrow:false,
+  infinite: true,
+  autoplay: true,
+  autoplaySpeed: 2000,
+   responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        centerMode: false,
+      }
+    }
    
-   // document.body.addEventListener('mousemove', function(e) {
-   //     let x = e.pageX - (window.innerWidth / 2);
-   //     let y = e.pageY - (window.innerHeight / 2);
-     
-   //     let animElements = document.querySelectorAll('.anim');
-   //     animElements.forEach(function(animElement) {
-   //       let factor = parseFloat(animElement.getAttribute('data-mouse-parallax'));
-   //       let newX = x * factor;
-   //       let newY = y * factor;
-     
-   //       animElement.style.transform = 'translate3d(' + newX + 'px, ' + newY + 'px, 0)';
-   //     });
-   //   });
-     
-   
+    
+  ]
+ 
+});
